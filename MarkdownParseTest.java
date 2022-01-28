1324e182f577be6051c083797b083eb929d5cc1d
jahyng <jwyang@ucsd.edu>
2022-01-28 13:58:26 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -49,7 +49,7 @@ public class MarkdownParseTest {
         Path fileName2 = Path.of("test-file2.md");
         String contents2 = Files.readString(fileName2);
         List links2 = List.of("https://something.com", "some-page.html");
-        assertEquals("checks result of test file 4", links2, MarkdownParse.getLinks(contents2));
+        assertEquals("checks result of test file 2", links2, MarkdownParse.getLinks(contents2));
 
         Path fileName3 = Path.of("test-file3.md");
         String contents3 = Files.readString(fileName3);
@@ -64,22 +64,22 @@ public class MarkdownParseTest {
         Path fileName5 = Path.of("test-file5.md");
         String contents5 = Files.readString(fileName5);
         List links5 = List.of();
-        assertEquals("checks result of test file 4", links5, MarkdownParse.getLinks(contents5));
+        assertEquals("checks result of test file 5", links5, MarkdownParse.getLinks(contents5));
 
         Path fileName6 = Path.of("test-file6.md");
         String contents6 = Files.readString(fileName6);
         List links6 = List.of("page.com");
-        assertEquals("checks result of test file 4", links6, MarkdownParse.getLinks(contents6));
+        assertEquals("checks result of test file 6", links6, MarkdownParse.getLinks(contents6));
 
         Path fileName7 = Path.of("test-file7.md");
         String contents7 = Files.readString(fileName7);
         List links7 = List.of();
-        assertEquals("checks result of test file 4", links7, MarkdownParse.getLinks(contents7));
+        assertEquals("checks result of test file 7", links7, MarkdownParse.getLinks(contents7));
 
         Path fileName8 = Path.of("test-file8.md");
         String contents8 = Files.readString(fileName8);
         List links8 = List.of();
-        assertEquals("checks result of test file 4", links8, MarkdownParse.getLinks(contents8));
+        assertEquals("checks result of test file 8", links8, MarkdownParse.getLinks(contents8));
 
     }
     
